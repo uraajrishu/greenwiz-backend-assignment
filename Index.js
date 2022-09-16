e53552a5e57221f1bb8690a1b41d7c3b33f54a79
urajrishuu <urajrishuu@gmail.com>
2022-09-16 22:39:07 +0530

    Backend Done

---
+++ Index.js
@@ -0,0 +1,43 @@
+const Axios = require('axios')
+const cheerio = require('cheerio')
+const fs = require('fs')
+const mime = require('mime-types')
+const http = require('https')
+
+async function getImagesFromUrl() {  //return images from url
+  const url = 'https://www.growpital.com/'
+  const response = await Axios.get(url)
+  // console.log(response.data)
+  const $ = cheerio.load(response.data);
+  let temp = [];
+  $("img").each((i, e) => {
+    temp.push($(e).attr('src'))
+  })
+  // return temp
+  return [...new Set(temp)]
+}
+
+async function downloadFile(url) {//Each images from url
+  try {
+    var resource = http.get(url, (res, err) => {
+      if (err) return console.log(err.message);
+      //console.log(res["_httpMessage"])
+      let contentType = mime.extension(res.headers["content-type"]);
+      let fileType = res.headers["content-type"].split("/")
+      if (fileType[0] != "image") return console.log("Invalid Url");
+      let rootePath = `./Downloads/imageFile_${new Date().getTime()}.${contentType}`
+      console.log("files downloaded successfully")
+      return res.pipe(fs.createWriteStream(rootePath))
+    })
+  } catch (err) {
+    return console.log(err)
+  }
+}
+
+async function root() {
+  let arr = await getImagesFromUrl()
+  for (let img of arr) {
+    await downloadFile(img)
+  }
+}
+root()
